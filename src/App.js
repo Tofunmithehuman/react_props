ba5ace8cd74a217937b72d783108db0c06f62b3e
Bolaji Oluwatofunmi Ashraf <Tofunmibolaji13@gmail.com>
2023-10-16 01:56:13 +0100

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import PlayerList from './Components/PlayerList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='p-5 bg-secondary'>
+            <h1 className='text-center m-2'>FIFA Player Cards</h1>
+            <PlayerList/>
+      </div>
   );
 }
 
---
+++ src/Components/Players.js
@@ -0,0 +1,42 @@
+//Importing all images
+import Halland from '../Media/Halland.png';
+import Kante from '../Media/Kante.png';
+import Saka from '../Media/Saka.png';
+import Osimhen from '../Media/Osimhen.jpg';
+
+
+//Creating array of objects
+export const players = [
+    {
+        name: `Erling Braut Haaland`,
+        team: "Manchester City",
+        nationality: "Norway",
+        jerseyNumber: 9,
+        age: 23,
+        image: Halland
+    },
+    {
+        name: `N'Golo Kanté`,
+        team: "Al-Ittihad ",
+        nationality: "France",
+        jerseyNumber: 7,
+        age: 32,
+        image: Kante
+    },
+    {
+        name: `Bukayo Temidayo Saka`,
+        team: "Arsenal",
+        nationality: "England",
+        jerseyNumber: 7,
+        age: 22,
+        image: Saka
+    },
+    {
+        name: `Victor James Osimhen`,
+        team: "Napoli",
+        nationality: " Nigeria",
+        jerseyNumber: 9,
+        age: 24,
+        image: Osimhen
+    }
+]
